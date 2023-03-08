3b7bd155f2fc21c30cb25d70324203efe16d5481
Kyle Neal <nealkyle5@gmail.com>
2023-03-07 19:29:49 -0600

    section 5 addl practive (light slider)

--- live_view_studio/lib/live_view_studio_web/live/light_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/light_live.ex
@@ -2,7 +2,7 @@ defmodule LiveViewStudioWeb.LightLive do
   use LiveViewStudioWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, brightness: 10)}
+    {:ok, assign(socket, brightness: 10, temp: 3000)}
   end
 
   def render(assigns) do
@@ -10,10 +10,11 @@ defmodule LiveViewStudioWeb.LightLive do
     <h1>Front Porch Light</h1>
     <div id="light">
       <div class="meter">
-        <span style={"width: #{@brightness}%"}>
+        <span style={"width: #{@brightness}%;background:#{temp_color(@temp)}"}>
           <%= @brightness %>%
         </span>
       </div>
+      <h4>Temperature Adjustment</h4>
       <button phx-click="off">
         <img src="/images/light-off.svg" alt="" />
       </button>
@@ -29,8 +30,36 @@ defmodule LiveViewStudioWeb.LightLive do
       <button phx-click="random">
         <img src="/images/fire.svg" alt="" />
       </button>
+      <form phx-change="slider_adjusted">
+        <input type="range" min="0" max="100" name="brightness" value="50" />
+      </form>
+      <form phx-change="temp_changed">
+        <h4>Light Temperature</h4>
+        <div class="temps">
+          <%= for temp <- ["3000", "4000", "5000"] do %>
+            <div>
+              <input
+                type="radio"
+                id={temp}
+                name="temp"
+                value={temp}
+                checked={temp == @temp}
+              />
+              <label for={temp}><%= temp %></label>
+            </div>
+          <% end %>
+        </div>
+      </form>
     </div>
     """
+  end
+
+  def handle_event("temp_changed", %{"temp" => temp} = _payload, socket) do
+    {:noreply, assign(socket, temp: String.to_integer(temp))}
+  end
+
+  def handle_event("slider_adjusted", %{"brightness" => brightness} = _payload, socket) do
+    {:noreply, assign(socket, brightness: String.to_integer(brightness))}
   end
 
   def handle_event("on", _payload, socket) do
@@ -52,4 +81,8 @@ defmodule LiveViewStudioWeb.LightLive do
   def handle_event("random", _payload, socket) do
     {:noreply, assign(socket, :brightness, :rand.uniform(100))}
   end
+
+  defp temp_color(3000), do: "#F1C40D"
+  defp temp_color(4000), do: "#FEFF66"
+  defp temp_color(5000), do: "#99CCFF"
 end
