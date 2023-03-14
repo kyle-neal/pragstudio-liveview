58c4b2ca23c13ff2b6f4adaa61f7c441721efa62
Kyle Neal <nealkyle5@gmail.com>
2023-03-14 10:05:15 -0500

    sec 12: sorting

--- live_view_studio/lib/live_view_studio_web/live/donations_live.ex
+++ live_view_studio/lib/live_view_studio_web/live/donations_live.ex
@@ -4,13 +4,34 @@ defmodule LiveViewStudioWeb.DonationsLive do
   alias LiveViewStudio.Donations
 
   def mount(_params, _session, socket) do
-    donations = Donations.list_donations()
-
-    socket =
-      assign(socket,
-        donations: donations
-      )
-
     {:ok, socket}
+  end
+
+  def handle_params(params, _uri, socket) do
+    sort_by = (params["sort_by"] || "id") |> String.to_atom()
+    sort_order = (params["sort_order"] || "desc") |> String.to_atom()
+    options = %{sort_by: sort_by, sort_order: sort_order}
+    donations = Donations.list_donations(options)
+    {:noreply, assign(socket, donations: donations, options: options)}
+  end
+
+  attr :sort_by, :atom, required: true
+  attr :options, :map, required: true
+
+  def sort_link(assigns) do
+    ~H"""
+    <.link patch={
+      ~p"/donations?#{%{sort_by: @sort_by, sort_order: flip_sort_order(@options.sort_order)}}"
+    }>
+      <%= render_slot(@inner_block) %>
+    </.link>
+    """
+  end
+
+  defp flip_sort_order(sort_order) do
+    case sort_order do
+      :asc -> :desc
+      :desc -> :asc
+    end
   end
 end
